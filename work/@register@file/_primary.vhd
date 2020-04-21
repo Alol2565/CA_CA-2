b6b6c6613a5a361cebee5de5c9f5f34815bfe74f
hamidsalemi77 <48052326+hamidsalemi77@users.noreply.github.com>
2020-04-22 03:55:11 +0430

    make progect and regsterfile

--- work/@register@file/_primary.vhd
+++ work/@register@file/_primary.vhd
@@ -4,14 +4,12 @@ entity RegisterFile is
     port(
         clk             : in     vl_logic;
         rst             : in     vl_logic;
-        WinEn           : in     vl_logic;
         RegWrite        : in     vl_logic;
-        ReadReg1        : in     vl_logic_vector(1 downto 0);
-        ReadReg2        : in     vl_logic_vector(1 downto 0);
-        WriteReg        : in     vl_logic_vector(1 downto 0);
-        WinData         : in     vl_logic_vector(1 downto 0);
-        WriteData       : in     vl_logic_vector(15 downto 0);
-        ReadData1       : out    vl_logic_vector(15 downto 0);
-        ReadData2       : out    vl_logic_vector(15 downto 0)
+        ReadReg1        : in     vl_logic_vector(4 downto 0);
+        ReadReg2        : in     vl_logic_vector(4 downto 0);
+        WriteReg        : in     vl_logic_vector(4 downto 0);
+        WriteData       : in     vl_logic_vector(31 downto 0);
+        ReadData1       : out    vl_logic_vector(31 downto 0);
+        ReadData2       : out    vl_logic_vector(31 downto 0)
     );
 end RegisterFile;
