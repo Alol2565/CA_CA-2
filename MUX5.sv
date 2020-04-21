82faf8050bb831b6d7996cd980694105f4f1b165
Ali Saei <alisaei90@gmail.com>
2020-04-22 04:00:53 +0430

    Update MUX5.sv

--- MUX5.sv
+++ MUX5.sv
@@ -1,6 +1,6 @@
 `timescale 1ns/1ns
-module Mux32(output	[31:0]	y,input [31:0]i0,i1,input sel);
-	reg	[31:0]	out; 	
+module Mux32(output	[4:0]	y,input [4:0]i0,i1,input sel);
+	reg	[4:0]	out; 	
 assign	y = out;	
 always @({sel,i0,i1})	begin
 	if (sel == 0) out = i0;
