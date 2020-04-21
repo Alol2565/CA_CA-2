a212418111ae2d4ad0ec83b4a8311b10ab802166
Ali Saei <alisaei90@gmail.com>
2020-04-22 03:43:04 +0430

    x

--- work/se16to32/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity se16to32 is
-    port(
-        \in\            : in     vl_logic_vector(15 downto 0);
-        clk             : in     vl_logic;
-        \out\           : out    vl_logic_vector(31 downto 0)
-    );
-end se16to32;
