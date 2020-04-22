04ebe7181caa08f59ce17e0dc7b26094e2864c64
hamidsalemi77 <48052326+hamidsalemi77@users.noreply.github.com>
2020-04-23 03:43:02 +0430

    h1

--- datamemory.sv
+++ datamemory.sv
@@ -1,5 +1,5 @@
 `timescale 1ns/1ns
-module datamem(input[31:0] address, writeData,memRead, memWrite, output[31:0] readdata);
+module datamem(input[31:0] address, writeData,input memRead, memWrite, output[31:0] readdata);
   reg[31:0] daTa;
   reg [31:0] memoryData[0:16383];
    initial
@@ -8,9 +8,9 @@ module datamem(input[31:0] address, writeData,memRead, memWrite, output[31:0] re
   end
   always@(posedge memRead,posedge memWrite )begin
     if(memRead)
-      daTa = memoryData[address << 2];
+      daTa = memoryData[address >> 2];
     if(memWrite)
-      memoryData[address << 2] = writeData;
+      memoryData[address >> 2] = writeData;
   end
   assign readdata = daTa;
 endmodule
--- test.sv
+++ test.sv
@@ -20,7 +20,7 @@ end
 
   initial begin
    rst =  1;
-   #20
+   #200
    rst = 0;
   // #30
   // RegWrite = 1;
