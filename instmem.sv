5c4489af425eac8a3e9ae48b3c4cdbd7b7465ff4
hamidsalemi77 <48052326+hamidsalemi77@users.noreply.github.com>
2020-04-22 15:33:49 +0430

    Update instmem.sv

--- instmem.sv
+++ instmem.sv
@@ -7,7 +7,7 @@ module instmem(input[31:0] address, input clk,rst, output[31:0] inst);
    $readmemb ("inst.data", memoryData);
   end
   always@(posedge clk)begin
-      daTa = memoryData[address << 2];
+      daTa = memoryData[address >> 2];
   end
   assign inst = daTa;
 endmodule
